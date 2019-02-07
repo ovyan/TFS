92c0300e040ee4d25f7a72f03401ba4fd7a5367f
Unknown <ovyan13@ya.ru>
2019-02-07 14:46:33 +0300

    project created

---
+++ TFS/TFS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TFS
+//
+//  Created by Mike Ovyan on 07/02/2019.
+//  Copyright © 2019 Mike Ovyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
