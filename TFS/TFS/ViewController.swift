e12553412dbd4342717d937d3a5f63bdee6c7a09
Unknown <ovyan13@ya.ru>
2019-02-09 15:19:40 +0300

    printing methods

--- TFS/TFS/ViewController.swift
+++ TFS/TFS/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        out.printer()
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        out.printer()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        out.printer()
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        out.printer()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        out.printer()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        out.printer()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        out.printer()
+    }
 }
-
---
+++ TFS/TFS/output.swift
@@ -0,0 +1,46 @@
+//
+//  output.swift
+//  TFS
+//
+//  Created by Mike Ovyan on 08/02/2019.
+//  Copyright © 2019 Mike Ovyan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public let out = Out(with: .inactive)
+
+public final class Out {
+    var prevState: UIApplication.State
+    
+    public func printer(_ caller: String = #function) {
+        #if LOG_ENABLED
+            let currState = UIApplication.shared.applicationState
+            let currStateString = stateToString(with: currState)
+            let prevStateString = stateToString(with: prevState)
+        
+            if currStateString != prevStateString {
+                print("Application moved from \(prevStateString) to \(currStateString): [\(caller)]")
+                prevState = currState
+            } else {
+                print("Application is at \(prevStateString): \(caller)")
+            }
+        #endif
+    }
+    
+    private func stateToString(with state: UIApplication.State) -> String {
+        switch state {
+        case .active:
+            return "active"
+        case .background:
+            return "background"
+        case .inactive:
+            return "inactive"
+        }
+    }
+    
+    public init(with state: UIApplication.State) {
+        prevState = state
+    }
+}
