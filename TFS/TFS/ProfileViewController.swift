02a0d9d433a29099d87a9abdce0daf28ee4918ed
Unknown <ovyan13@ya.ru>
2019-02-13 22:52:37 +0300

    storyboard

---
+++ TFS/TFS/ProfileViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  TFS
+//
+//  Created by Mike Ovyan on 07/02/2019.
+//  Copyright © 2019 Mike Ovyan. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        out.printer()
+    }
+}
