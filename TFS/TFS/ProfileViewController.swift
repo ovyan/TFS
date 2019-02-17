b501c8c7b2066d8d4841feab70180561c0c9d4c9
Unknown <ovyan13@ya.ru>
2019-02-17 16:51:35 +0300

    storyboard is cool

--- TFS/TFS/ProfileViewController.swift
+++ TFS/TFS/ProfileViewController.swift
@@ -9,8 +9,43 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
+    // MARK: - Outlets
+    
+    @IBOutlet var photoButton: UIButton!
+    @IBOutlet var userAvatar: UIImageView!
+    @IBOutlet var editButton: UIButton!
+    
+    @IBAction func handlePhotoTap(_ sender: Any) {
+        print("Choose profile pic")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        out.printer()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        setupAvatar()
+        setupButton()
+    }
+    
+    func setupAvatar() {
+        let cornerRadius = photoButton.bounds.width / 2
+        let inset = CGFloat(16)
+        
+        photoButton.layer.cornerRadius = cornerRadius
+        photoButton.clipsToBounds = true
+        photoButton.imageView?.contentMode = .scaleAspectFit
+        photoButton.imageEdgeInsets = UIEdgeInsets(top: inset, left: inset, bottom: inset, right: inset)
+        
+        userAvatar.layer.cornerRadius = cornerRadius
+        userAvatar.clipsToBounds = true
+    }
+    
+    func setupButton() {
+        editButton.layer.borderWidth = 1
+        editButton.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        editButton.layer.cornerRadius = 10
+        editButton.clipsToBounds = true
     }
 }
