ff9690f85385d15667bf12d500501fc8fce66add
Unknown <ovyan13@ya.ru>
2019-02-18 23:15:25 +0300

    photo adding

--- TFS/TFS/ProfileViewController.swift
+++ TFS/TFS/ProfileViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     // MARK: - Outlets
     
     @IBOutlet var photoButton: UIButton!
@@ -17,10 +17,17 @@ class ProfileViewController: UIViewController {
     
     @IBAction func handlePhotoTap(_ sender: Any) {
         print("Choose a profile pic")
+        present(alert, animated: true, completion: nil)
     }
+    
+    let imagePC = UIImagePickerController()
+    
+    let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        imagePC.delegate = self
+        setAlert()
         
         // On this step not all of the constraints are set to their final values
         // Thus, a frame of the button is calculated without considering other constraints
@@ -34,7 +41,7 @@ class ProfileViewController: UIViewController {
         super.viewDidAppear(true)
         
         // On this step all of the constrainst have its final values
-        // Hence, the frame has been recalculated and now has its final value too
+        // Hence, the frame has been recalculated for the specific device and now has its final value too
         guard let editButtonFrame = editButton?.frame else {
             return
         }
@@ -57,6 +64,17 @@ class ProfileViewController: UIViewController {
         print("Frame is in \(#function) with properties: \(editButtonFrame)")
     }
     
+    private func presentImagePicker(with type: UIImagePickerController.SourceType) {
+        let picker = UIImagePickerController()
+        picker.sourceType = type
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        present(picker, animated: true)
+    }
+    
+    // MARK: - Setup
+    
     func setupAvatar() {
         let cornerRadius = photoButton.bounds.width / 2
         let inset = CGFloat(16)
@@ -75,5 +93,35 @@ class ProfileViewController: UIViewController {
         editButton.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
         editButton.layer.cornerRadius = 10
         editButton.clipsToBounds = true
+    }
+    
+    func setAlert() {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let chooseFromGallery = UIAlertAction(title: "Choose from Library", style: .default) { [weak self] _ in
+                self?.presentImagePicker(with: .photoLibrary)
+            }
+            alert.addAction(chooseFromGallery)
+        }
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let takePhoto = UIAlertAction(title: "Make a photo", style: .default) { [weak self] _ in
+                self?.presentImagePicker(with: .camera)
+            }
+            alert.addAction(takePhoto)
+        }
+        
+        let cancel = UIAlertAction(title: "Close", style: .cancel) { [weak self] _ in
+            self?.dismiss(animated: true)
+        }
+        alert.addAction(cancel)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        let pickedImage = info[.editedImage] ?? info[.originalImage]
+        
+        if let pickedImage = pickedImage as? UIImage {
+            userAvatar.image = pickedImage
+        }
+        dismiss(animated: true, completion: nil)
     }
 }
